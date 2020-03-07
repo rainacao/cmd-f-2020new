b4f0f187290cef698f8566f74f58b6b573e7338b
Yiyuan Sun <50027648+nusnauyiy@users.noreply.github.com>
2020-03-07 17:24:50 -0600

    started activities xml

--- app/src/main/java/com/example/cmdf2020new/MainActivity.java
+++ app/src/main/java/com/example/cmdf2020new/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cmdf2020new;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onButtonClicked(View v) {
+        Intent intent = new Intent(this, Plant.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/cmdf2020new/Plant.java
@@ -0,0 +1,14 @@
+package com.example.cmdf2020new;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Plant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant);
+    }
+}
