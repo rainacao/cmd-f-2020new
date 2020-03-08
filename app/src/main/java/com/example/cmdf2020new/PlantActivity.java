0db2364bfbdaac89ad2409e094bc3eb2a6f47f71
Raina Cao <45450525+rainacao@users.noreply.github.com>
2020-03-08 01:13:13 -0800

    made plant activity, linked shit up

--- app/src/main/java/com/example/cmdf2020new/PlantActivity.java
+++ app/src/main/java/com/example/cmdf2020new/PlantActivity.java
@@ -2,7 +2,9 @@ package com.example.cmdf2020new;
 
 import  androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PlantActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class PlantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant);
+    }
+    public void goMakeTask(View v){
+        Intent intent = new Intent(this, AddEditTaskActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToCalendar(View v){
+        Intent intent = new Intent(this, TaskCalendarActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToSettings(View v){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
