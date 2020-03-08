02cd270047998fc16ca84ef310587e234a8867bc
Helen Yu <helenmqyu@gmail.com>
2020-03-07 23:46:39 -0800

    calendar worky god blessy

--- app/src/main/java/com/example/cmdf2020new/MainActivity.java
+++ app/src/main/java/com/example/cmdf2020new/MainActivity.java
@@ -4,10 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
