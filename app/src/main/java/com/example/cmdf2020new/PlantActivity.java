a1abca3546f4f356320c3278cb7bcffffdf368f4
Raina Cao <45450525+rainacao@users.noreply.github.com>
2020-03-08 03:26:46 -0700

    tweaked plant things to change the image hopefully. idk if it works tho LMAO no sleep gang

--- app/src/main/java/com/example/cmdf2020new/PlantActivity.java
+++ app/src/main/java/com/example/cmdf2020new/PlantActivity.java
@@ -5,14 +5,20 @@ import  androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import model.plant.Plant;
 
 public class PlantActivity extends AppCompatActivity {
+
+    Plant testPlant = new Plant();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant);
     }
+
     public void goMakeTask(View v){
         Intent intent = new Intent(this, AddEditTaskActivity.class);
         startActivity(intent);
@@ -26,5 +32,20 @@ public class PlantActivity extends AppCompatActivity {
     public void goToSettings(View v){
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    final ImageView imageView = (ImageView) findViewById(R.id.plantImg);
+
+    public void changePlantImg(Plant p){
+        int level = p.getCurrentLevel();
+        if (level == 2){
+            imageView.setImageResource(R.drawable.plant_img1);
+        }
+        else if (level == 4){
+            imageView.setImageResource(R.drawable.plant_img2);
+        }
+        else if (level == 6){
+            imageView.setImageResource(R.drawable.plant_img3);
+        }
     }
 }
