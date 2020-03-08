1de3459c4ff13802ce219a7c6be7c4006dd66d2b
Helen Yu <helenmqyu@gmail.com>
2020-03-07 22:43:09 -0800

    got rid of that test module and tryna make the calendar button work (but it aint)

--- app/src/main/java/com/example/cmdf2020new/MainActivity.java
+++ app/src/main/java/com/example/cmdf2020new/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
