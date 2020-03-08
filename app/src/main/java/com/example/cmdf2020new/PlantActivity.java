0e2d07fcf375d82cac1d330d5d54ce75fc058227
Raina Cao <45450525+rainacao@users.noreply.github.com>
2020-03-08 08:59:56 -0700

    made level change textboxes

--- app/src/main/java/com/example/cmdf2020new/PlantActivity.java
+++ app/src/main/java/com/example/cmdf2020new/PlantActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import database.SystemDatabase;
 import model.plant.Plant;
@@ -13,18 +14,37 @@ import model.plant.Plant;
 public class PlantActivity extends AppCompatActivity {
 
     private ImageView image;   // the plant and its evolution
+    private TextView lvlNow; //current level
+    private TextView lvlNext; //next level
 
     Plant testPlant0 = new Plant(0, 1);
     Plant testPlant1 = new Plant(2, 1);
     Plant testPlant2 = new Plant(5, 4);
-    Plant testPlant3 = new Plant(6, 1);
+    Plant testPlant3 = new Plant(6, 4);
     Plant plant;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant);
         plant = SystemDatabase.myDB.getPlant();
-        updatePlantImage(plant);
+        getLvlNow(testPlant3);
+        getLvlNext(testPlant3);
+        //lvlNow = (TextView) findViewById(R.id.initLevelText);
+        //lvlNext = (TextView) findViewById(R.id.nextLevelText);
+        updatePlantImage(testPlant3);
+
+    }
+
+    public TextView getLvlNow(Plant p) {
+        lvlNow = (TextView) findViewById(R.id.initLevelText);
+        lvlNow.setText(Integer.toString(p.getCurrentLevel()));
+        return lvlNow;
+    }
+
+    public TextView getLvlNext(Plant p) {
+        lvlNext = (TextView) findViewById(R.id.nextLevelText);
+        lvlNext.setText(Integer.toString(p.getCurrentLevel() + 1));
+        return lvlNext;
     }
 
     public void goMakeTask(View v) {
