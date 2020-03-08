3758b2bb62c6bfc44d7b4e492d7f9725144004be
Helen Yu <helenmqyu@gmail.com>
2020-03-08 01:30:44 -0800

    now u can view the tasks on a given day. wow!

--- app/src/main/java/com/example/cmdf2020new/TaskCalendarActivity.java
+++ app/src/main/java/com/example/cmdf2020new/TaskCalendarActivity.java
@@ -25,15 +25,22 @@ public class TaskCalendarActivity extends AppCompatActivity {
         simpleCalendarView.setSelectedWeekBackgroundColor(Color.RED);
         simpleCalendarView.setWeekSeparatorLineColor(Color.GREEN);
 
+        final Button viewTasksButton = findViewById(R.id.viewTasksButton);
         final Button makeTaskButton = findViewById(R.id.makeTaskButton);
 
         simpleCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
                 //Toast.makeText(getApplicationContext(), dayOfMonth + "/" + (month + 1) + "/" + year, Toast.LENGTH_LONG).show();
+                viewTasksButton.setVisibility(View.VISIBLE);
                 makeTaskButton.setVisibility(View.VISIBLE);
             }
         });
+    }
+
+    public void goToTasks(View v) {
+        Intent intent = new Intent (this, TasksActivity.class);
+        startActivity(intent);
     }
 
     public void goMakeTask(View v) {
